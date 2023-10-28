727f92963337b88758c7cf7c70e3a260041e492f
Anagha <anaghakrishna92@gmail.com>
2023-10-27 23:21:04 -0400

    add data files to git ignore

---
+++ akrishna311/ispcr/step_one.py
@@ -0,0 +1,15 @@
+import subprocess
+
+def step_one(primer_file: str, assembly_file: str) -> list[list[str]]:
+    blast_output = subprocess.run(["blastn", "-task", "blastn-short", "-query", primer_file, "-subject", assembly_file, "-outfmt", '6 std qlen'], \
+                   capture_output=True, \
+                   text=True
+                   )
+    filtered_blast_output = subprocess.run("awk '{if ($3 >= 80.000 && $4 == $13 ) print $0;}'", \
+                                           capture_output=True, \
+                                           text=True, \
+                                           shell=True, \
+                                           input=blast_output.stdout
+                                           #executable="/bin/bash"
+                                           )
+    print(filtered_blast_output.stdout)
